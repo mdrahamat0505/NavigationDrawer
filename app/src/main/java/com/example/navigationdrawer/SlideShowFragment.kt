07b61b645765e685a7f1a0d51e555940439f785c
mdrahamat0505 <mdrahamat0505@gmail.com>
2023-05-31 23:13:48 +0600

    create navigation

---
+++ app/src/main/java/com/example/navigationdrawer/SlideShowFragment.kt
@@ -0,0 +1,11 @@
+package com.example.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SlideShowFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slide_show_fragment)
+    }
+}
